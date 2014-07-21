da8537c99b7b21dcccde3b1f9e3a2c0133a856f6
giangdongmai <prdonggiang@gmail.com>
2014-07-20 22:39:25 -0500

    template main
    done!

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -2,33 +2,44 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin - Bootstrap Admin Template</title>
+    <title>Admin - Thuna.vn</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="{{Asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
-
     <!-- Custom CSS -->
     <link href="{{Asset('assets/css/sb-admin.css')}}" rel="stylesheet" type="text/css">
-
     <!-- Morris Charts CSS -->
     <link href="{{Asset('assets/css/morris.css')}}" rel="stylesheet" type="text/css">
-
     <!-- Custom Fonts -->
-    <link href="{{Asset('assets/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{Asset('assets/font-awesome-4.1.0/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
 
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <script src="{{Asset('assets/js/jquery-1.11.0.js')}}"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{{Asset('assets/js/bootstrap.min.js')}}"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="{{Asset('assets/js/plugins/morris/raphael.min.js')}}"></script>
+    <script src="{{Asset('assets/js/plugins/morris/morris.min.js')}}"></script>
+    <script src="{{Asset('assets/js/plugins/morris/morris-data.js')}}"></script>
+    
 </head>
 
 <body>
-
     <div id="wrapper">
-
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <!-- Brand and toggle get grouped for better mobile display -->
@@ -39,7 +50,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">SB Admin</a>
+                <a class="navbar-brand" href="#">Site Admin</a>
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
@@ -462,17 +473,6 @@
 
     </div>
     <!-- /#wrapper -->
-
-    <!-- jQuery Version 1.11.0 -->
-    <script src="js/jquery-1.11.0.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="js/plugins/morris/raphael.min.js"></script>
-    <script src="js/plugins/morris/morris.min.js"></script>
-    <script src="js/plugins/morris/morris-data.js"></script>
 
 </body>
 
